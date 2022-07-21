a58d3bb5a32ab6a40d3eabf59afb5303956341d4
Djinnu <dzinnu.hunt@gmail.com>
2022-07-21 23:04:39 +0300

    checking whether a given number is a prime without looping all the way to the number

---
+++ 6kyu/Is-a-number-prime.js
@@ -0,0 +1,28 @@
+//my first solution:
+function isPrime(num) {
+    if (num == 2 || num == 3) {
+      return true;
+    }
+    if (num <= 1 || num % 2 === 0 || num % 3 === 0) {
+      return false; 
+    }
+    
+    for (let i = 5; i * i <= num ; i+=6) {
+      if (num % i === 0 || num % (i + 2) === 0) {
+        return false;
+      }
+    }
+      
+    return true;
+}
+//better solution:
+function isPrime(num) {
+    if (num < 2) return false;
+    const limit = Math.sqrt(num);
+    for (let i = 2; i <= limit; ++i) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+}
