081ec5d8f8a4c944e2dbf5271ba0ffd66071487c
Djinnu <dzinnu.hunt@gmail.com>
2023-04-27 15:29:35 +0300

    class

---
+++ Barking-mad.js
@@ -0,0 +1,28 @@
+//my first solution:
+function Dog (breed) {
+    this.breed = breed;
+}
+  
+var snoopy = new Dog("Beagle");
+  
+snoopy.bark = function() {
+    return "Woof";
+};
+  
+var scoobydoo = new Dog("Great Dane");
+  scoobydoo.bark = function() {
+    return "Woof"
+}
+//better one:
+class Dog {
+    constructor(breed) {
+        this.breed = breed;
+    }
+    
+    bark() {
+        return "Woof";
+    }
+}
+
+let snoopy = new Dog("Beagle");
+let scoobydoo = new Dog("Great Dane");  
