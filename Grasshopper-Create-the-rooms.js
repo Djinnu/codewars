46d2b9959d01abfe6760cc99622ed18b7ef6c1dd
Djinnu <dzinnu.hunt@gmail.com>
2023-05-28 13:10:08 +0300

    creating a obj

---
+++ Grasshopper-Create-the-rooms.js
@@ -0,0 +1,17 @@
+var rooms = {
+    room1: {
+      name: "kitchen",
+      description: "eating area",
+      completed: true
+    },
+    room2: {
+      name: "living room",
+      description: "chilling area",
+      completed: true
+    },
+    room3: {
+      name: "nursery",
+      description: "for future children",
+      completed: false
+    }
+}
