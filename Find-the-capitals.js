fa697b8287edc311cec2731e5d4a95531c60a7a0
Djinnu <dzinnu.hunt@gmail.com>
2023-06-18 02:31:51 +0300

    looping

---
+++ Find-the-capitals.js
@@ -0,0 +1,11 @@
+var capitals = function (word) {
+	let resultArr = []
+  
+  for(let i = 0; i < word.length; i++) {
+    if(word[i] === word[i].toUpperCase()) {
+      resultArr.push(i)
+    }
+  }
+  
+  return resultArr
+};
