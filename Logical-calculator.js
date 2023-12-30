f1a086f724faddbcbfd1021561cde40219359f84
Djinnu <dzinnu.hunt@gmail.com>
2023-12-30 20:09:00 +0200

    && || !=

--- Logical-calculator.js
+++ Logical-calculator.js
@@ -1,5 +1,53 @@
-function logicalCalc(array, op){
-    if(op === 'AND')return array.every(v => v);
-    else if(op === 'OR')return array.some(v => v);
-    else return !!array.reduce((s, v) => s ^ v);
+//my first solution:
+
+function logicalCalc(array, op){ 
+    let arr = [array[0]]
+    
+    if(op === "AND") {
+      if(array.includes(false)) {
+        return false
+      } else {
+        return true
+      }
+    } else if(op === "OR") {
+      for(let i = 1; i < array.length; i++) {
+        if(arr[i-1] === false && array[i] === false) {
+          arr.push(false)
+        } else {
+          arr.push(true)
+        }
+      }
+      return arr[arr.length-1]
+    } else if(op === "XOR") {
+      for(let i = 1; i < array.length; i++) {
+        if(array[i] === arr[i-1]) {
+          arr.push(false)
+        } else {
+          arr.push(true)
+        }
+      }
+      return arr[arr.length-1]
+    }
+  }
+
+//better solution:
+function logicalCalc(array, op)
+{ 
+  var result = array[0];
+  for(var i = 1; i < array.length; i++)
+  {
+    if(op == "AND")
+    {
+      result = result && array[i];
+    }
+    if(op == "OR")
+    {
+      result = result || array[i];
+    }
+    if(op == "XOR")
+    {
+      result = result != array[i];
+    }
+  }
+  return result;
 }
